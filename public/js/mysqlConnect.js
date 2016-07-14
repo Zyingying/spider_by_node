0c6c6d9963b3a4b7db170501c4d1a7fc61e15d37
Zyingying <494971645@qq.com>
2016-07-15 00:25:14 +0800

    Signed-off-by: Zyingying <494971645@qq.com>

--- public/js/mysqlConnect.js
+++ public/js/mysqlConnect.js
@@ -2,7 +2,7 @@
 * @Author: Yingying
 * @Date:   2016-07-04 00:27:29
 * @Last Modified by:   Yingying
-* @Last Modified time: 2016-07-15 00:09:25
+* @Last Modified time: 2016-07-15 00:23:29
 */
 var main = document.querySelector('#main');
 
@@ -12,13 +12,12 @@ $.get('http://localhost:3000/', function(data) {
     
          
 });
-// 思路：将数据插入页面
+// 思路：将数据插入页面 
 function draw(num,data){
     var div  = document.createElement("div"),
         node = document.createTextNode(data[num].Url);
 
     div.appendChild(node);
-
     var main = document.getElementById("main");
     element.appendChild(div);
 }
