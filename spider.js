4e8b398f0770f1f4042d69da38f036c46e1cd6c3
unknown <you@example.com>
2016-07-14 17:40:08 +0800

    delect some test js and update spider.js

--- spider.js
+++ spider.js
@@ -1,8 +1,8 @@
 /* 
 * @Author: Yingying
 * @Date:   2016-05-24 23:45:59
-* @Last Modified by:   Marte
-* @Last Modified time: 2016-07-14 11:28:21
+* @Last Modified by:   Yingying
+* @Last Modified time: 2016-07-14 17:25:47
 */
 
 "use strict";
@@ -17,6 +17,13 @@ var app = express();
 var url    =  "http://zyingying.github.io",
 		author =  "zyingying",
 		data   =  "" ;
+		// 连接池
+let connection = mysql.createConnection({     
+			host     : '127.0.0.1',
+			user     : 'root',
+			password : 'lwy1234',
+			database : 'nodesample'
+}); 
 
 // 创建一个请求
 var req = http.request(url, function(res){
@@ -42,13 +49,10 @@ var req = http.request(url, function(res){
 								"Title": match[4]
 						});
 				}
-				// console.log(res);
-				console.log("1++++++++++"+res[0].Url)                      
-				console.log("2+++++++++++++++++"+res[0].Title) ;
-				let len = res.length;
-				for(let j = 0; j < len; j++){
-					SelectData(res[j]);
-				}
+
+				main(res);
+				console.log(res);
+				// }
 				// connection.end();
 		});
 });
@@ -57,132 +61,130 @@ var req = http.request(url, function(res){
 req.end();
 
 
-// var connectMysql = function(){
-// 连接数据库的函数，里面包括四个函数：增删查改
+	//查询
+	const SelectData = (data,fun) => {
 
-		var connection = mysql.createConnection({     
-					host     : '127.0.0.1',
-					user     : 'root',
-					password : 'root',
-					database : 'nodesample'
-		}); 
+			// 查询
+			var  userGetSql = 'SELECT * FROM catalog WHERE Url = \'' + data.Url + ' \'';
+			console.log(userGetSql);
+			// 查
+			connection.query(userGetSql,function (err, result) {
+							if(err){
+								console.log('[SELECT ERROR] - ',err.message);
+								return;
+							}        
+						 console.log('--------------------------SELECT----------------------------');
+						 console.log('result----->',result);
 
-		connection.connect();
-
-			//查询
-			const SelectData = (data) => {
-					// 查询
-					var  userGetSql = 'SELECT * FROM catalog WHERE Url = \'' + data.Url + ' \'';
-					console.log(userGetSql);
-					// 查
-					connection.query(userGetSql,function (err, result) {
-									if(err){
-										console.log('[SELECT ERROR] - ',err.message);
-										return;
-									}        
-								 console.log('--------------------------SELECT----------------------------');
-								 console.log('result----->',result);
-								 if (result.length !== 0) {
-											// 数据库中有相同的东西，更新
-											Update(author,data.Title,data.Url);
-								 }else{
-										// 找不到相同的数据，则是新数据，插入到数据库中
-											Insert(data);
-								 }
-					});
-			}
+						 fun(null, result);
+			});
+			// connection.end();
+	}
 
 		// 增加
-		const Insert = (data) => {
-				var  userAddSql = 'INSERT INTO catalog(Author,Title,Url) VALUES(?,?,?)';
-				var  userAddSql_Params = [author,data.Title,data.Url];
-				console.log('插入数据中。。。。',userAddSql_Params);
-				// //增
-				connection.query(userAddSql,userAddSql_Params,function (err, result) {
-								if(err){
-								 console.log('[INSERT ERROR] - ',err.message);
-								 return;
-								}        
-							 console.log('--------------------------INSERT----------------------------');
-							 console.log('INSERT ID:\n',result);        
-							 console.log('-----------------------------------------------------------------\n\n');  
-				});
+	const Insert = (data) => {
+
+			var  userAddSql = 'INSERT INTO catalog(Author,Title,Url) VALUES(?,?,?)';
+			//log: 取对象属性的时候,注意判断是否有值 
+			if(!data) return;
+			var  userAddSql_Params = [author,data.Title,data.Url];
+			console.log('插入数据中。。。。',userAddSql_Params);
+			// //增
+			connection.query(userAddSql,userAddSql_Params,function (err, result) {
+							if(err){
+							 console.log('[INSERT ERROR] - ',err.message);
+							 return;
+							}        
+						 console.log('--------------------------INSERT----------------------------');
+						 console.log('INSERT ID:\n',result);        
+						 console.log('-----------------------------------------------------------------\n\n');  
+			});
+
+	}
+
+
+	// 更新
+	const Update = (author,title,url) => {
+
+			var userModSql = 'UPDATE catalog SET Author = ? ,Title = ? WHERE Url = ?';
+			var userModSql_Params = [author,title,url];
+			//改
+			connection.query(userModSql,userModSql_Params,function (err, result) {
+				 if(err){
+							 console.log('[UPDATE ERROR] - ',err.message);
+							 return;
+				 }        
+				console.log('--------------------------UPDATE----------------------------');
+				console.log('UPDATE affectedRows',result.affectedRows);
+			});
+
+
+	}
+
+	const Delete = () => {
+
+			var  userDelSql = 'DELETE FROM catalog';
+			//删
+			connection.query(userDelSql,function (err, result) {
+							if(err){
+								console.log('[DELETE ERROR] - ',err.message);
+								return;
+							}        
+						 console.log('--------------------------DELETE----------------------------');
+						 console.log('DELETE affectedRows',result.affectedRows);
+						 console.log('-----------------------------------------------------------------\n\n');  
+			});
+	}
+ 
+	const main = (data) => {
+		
+		console.log('++++++++++++++++++++++++++++++++++++++',data)
+		let len = data.length,j;
+
+		for(j = 0 ; j < len; j++){
+
+				SelectData(data[j],function(err, result){
+					if (result.length !== 0) {
+							//log: 取对象属性的时候,注意判断是否有值 
+							// 数据库中有相同的东西，更新
+							!data && !data[j].Title && Update(author,data[j].Title,data[j].Url);
+					}else{
+							// 找不到相同的数据，则是新数据，插入到数据库中
+							Insert(data[j]);
+					}
+				})
 				
 		}
+			
+		app.use('/', express.static('public'));
 
-
-		// 更新
-		const Update = (author,title,url) => {
-
-				var userModSql = 'UPDATE catalog SET Author = ? ,Title = ? WHERE Url = ?';
-				var userModSql_Params = [author,title,url];
-				//改
-				connection.query(userModSql,userModSql_Params,function (err, result) {
-					 if(err){
-								 console.log('[UPDATE ERROR] - ',err.message);
-								 return;
-					 }        
-					console.log('--------------------------UPDATE----------------------------');
-					console.log('UPDATE affectedRows',result.affectedRows);
-					// console.log('-----------------------------------------------------------------\n\n');
+		app.get('/', function (req, res) {
+				SelectAll(function(err, result) {
+					res.send(result);
 				});
-		}
+		});
 
-		const Delete = () => {
+		app.listen(3000, function () {
+		    console.log('Example app listening on port 3000!');
+		});  
+	}
 
-				var  userDelSql = 'DELETE FROM catalog';
-				//删
-				connection.query(userDelSql,function (err, result) {
-								if(err){
-									console.log('[DELETE ERROR] - ',err.message);
-									return;
-								}        
-							 console.log('--------------------------DELETE----------------------------');
-							 console.log('DELETE affectedRows',result.affectedRows);
-							 console.log('-----------------------------------------------------------------\n\n');  
-				});
-		}
-	 
-																								 
-	 	const SelectAll = () => {
-				// 查询
-				var  userGetSql = 'SELECT * FROM catalog  ';
-				// 查
-				connection.query(userGetSql,function (err, result) {
-								if(err){
-									console.log('[SELECT ERROR] - ',err.message);
-									return;
-								}        
 
-							 console.log('--------------------------SELECT----------------------------');
-							 console.log('result----->',result);
+ 	const SelectAll = (done) => {
+			// 查询
+			var  userGetSql = 'SELECT * FROM catalog  ';
+			// 查
+			connection.query(userGetSql,function (err, result) {
+							if(err){
+								console.log('[SELECT ERROR] - ',err.message);
+								return done(err);
+							}        
 
-							 app.use('/', express.static('public'));
+						 console.log('--------------------------SELECT----------------------------');
+						 console.log('result----->',result);
 
-							 app.get('/', function (req, res) {
-									 res.send(result);
-							 });
+						 done(null, result);
+							
+			});
+	}
 
-							 app.listen(3000, function () {
-									 console.log('Example app listening on port 3000!');
-							 }); 
-								
-							// connectMysql();
-				});
-		}
-
-SelectAll();
-	// 获取所有的数据到前端
-	
-
-// app.use('/', express.static('public'));
-
-// app.get('/', function (req, res) {
-//     res.send("123");
-// });
-
-// app.listen(3000, function () {
-//     console.log('Example app listening on port 3000!');
-// });  
-
-// connection.end();
