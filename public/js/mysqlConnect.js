f78db9ffb99329ccf69328bbf133b72b2db73329
Zyingying <494971645@qq.com>
2016-07-15 00:18:18 +0800

    更新思路，明天待续

--- public/js/mysqlConnect.js
+++ public/js/mysqlConnect.js
@@ -2,7 +2,7 @@
 * @Author: Yingying
 * @Date:   2016-07-04 00:27:29
 * @Last Modified by:   Yingying
-* @Last Modified time: 2016-07-08 10:13:15
+* @Last Modified time: 2016-07-15 00:09:25
 */
 var main = document.querySelector('#main');
 
@@ -12,7 +12,7 @@ $.get('http://localhost:3000/', function(data) {
     
          
 });
-
+// 思路：将数据插入页面
 function draw(num,data){
     var div  = document.createElement("div"),
         node = document.createTextNode(data[num].Url);
